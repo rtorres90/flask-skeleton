bb988bc9b6d274c89c672daec472f45ba54e629c
rtorres90 <roberto1990z>
2016-02-03 23:09:10 -0300

    reformat code and logging

--- flask-app/www.py
+++ flask-app/www.py
@@ -1,3 +1,5 @@
+import logging
+from logging.handlers import RotatingFileHandler
 
 from mako.lookup import TemplateLookup
 
@@ -5,9 +7,11 @@ from app import app
 
 lookup = TemplateLookup('./templates')
 
+
 @app.route('/')
 def home():
     return lookup.get_template("index.html").render(message="Hello world")
+
 
 @app.route('/hello/<hello_message>')
 def say_hello(hello_message):
@@ -15,4 +19,9 @@ def say_hello(hello_message):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    formatter = logging.Formatter("[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s")
+    handler = RotatingFileHandler('/var/log/flask.log', maxBytes=10000000, backupCount=1)
+    handler.setLevel(logging.DEBUG)
+    handler.setFormatter(formatter)
+    app.logger.addHandler(handler)
+    app.run(debug=False, host='0.0.0.0', port=80)
